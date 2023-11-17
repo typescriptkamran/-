d742cc02f9685ae7999b986e15e76d9b64977914
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-11-17 06:05:34 +0500

    update

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This is Dashboard</div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import Link from "next/link"
 export default function Page() {
   return (
   <main>
-  <header>
-    about
-    blog
-    contact us
-    login
-  </header>
   
   <h1>Hello, Next.js!</h1>
   
